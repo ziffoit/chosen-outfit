64bc2a562df0262418ab52942efc691202cfc3ee
Cody <gallagher2021@gmail.com>
2021-07-23 16:11:28 -0600

    Added the flip css to the homepage

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -34,6 +34,11 @@ function Nav() {
               Login
             </Link>
           </li>
+          <li className="mx-1">
+            <Link to="/picUpload">
+              Upload
+            </Link>
+          </li>
         </ul>
       );
     }
